6a1c58be4cf8d87c2ecddf56dd76f196b3531d07
IsmaelDev13 <ismael-diaz8@hotmail.es>
2024-09-29 19:05:31 +0200

    footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,16 @@ import styles from "../styles/Home.module.css";
 import { Barlow } from "next/font/google";
 import { RiArrowDropRightLine } from "react-icons/ri";
 import Link from "next/link";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const Home: NextPage = () => {
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      router.replace("/coches");
+    }, 2000);
+  }, []);
   return (
     <div className="">
       <Head>
@@ -15,8 +23,11 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={" h-screen w-screen bg-[#3A5C78]"}>
-        <section className=" max-w-7xl mx-auto w-full flex flex-col items-center space-y-4">
+      <main className={` h-screen w-screen relative`}>
+        <div
+          className={`bg-[url("https://driverevel.com/images/files/home-hero/imagen-hero-home-MOB_2x.webp")] h-full w-full z-0 absolute top-0 right-0 left-0  bg-no-repeat bg-cover bg-center`}
+        />
+        <section className=" max-w-7xl mx-auto w-full flex flex-col items-center space-y-4 h-full z-10 absolute top-10">
           <h1 className="text-white text-[28px] font-semibold">
             El nuevo renting para particulares
           </h1>
@@ -45,16 +56,29 @@ const Home: NextPage = () => {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        <div className="flex items-center space-x-1">
+          <p>Developed by</p>{" "}
+          <a
+            href="https://www.linkedin.com/in/ismael-diazb/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            Ismael
+          </a>{" "}
+          <a
+            href="https://github.com/ismaeldev13"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/github-mark.png"
+              alt="Github Logo"
+              width={20}
+              height={20}
+            />
+          </a>
+        </div>
       </footer>
     </div>
   );
